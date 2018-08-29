ebc85cf6dce2708bbbb651dfc9b41fd75d1ecfe0
Randall Johnons <rjohns29@my.hpu.edu>
2018-08-28 23:39:47 -1000

    Add commit

---
+++ hw01/factorial.h
@@ -0,0 +1,7 @@
+// proprocessor directives
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+int addFact(int x);
+
+#endif
---
+++ hw01/main.cc
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include "factorial.h"
+
+int main(int argc, char** argv) {
+	printf("%d\n", addFact(5));
+	return 0;
+}
