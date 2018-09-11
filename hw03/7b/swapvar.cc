286dc85c85d040589aa6b0c22854cde210479c7b
U-DESKTOP-CA9NQBD\randa <rjohns29@my.hpu.edu>
2018-09-11 11:54:22 -1000

    updated homework

--- hw03/7b/swapvar.cc
+++ hw03/7b/swapvar.cc
@@ -3,7 +3,7 @@
 // input: 2 pointers, each pointing to an integer.
 void swapVariables(int* a, int* b) {
     // if the value in a is greater than the value in b.
-    if (a > b) {
+    if (*a > *b) {
         // declare temporary variable
         int temp = 0;
         // the data pointed by b is stored inside the temp variable.
